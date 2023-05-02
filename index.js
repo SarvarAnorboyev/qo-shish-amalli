0f71c705078216bc974b93ae2266bc0607f16b8f
SarvarAnorboyev <sarvaranorboyev95@gmail.com>
2023-05-02 18:33:25 +0500

    fix

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let a =+prompt("birinchi raqamni kiriting")
+
+let b =+prompt("ikkinchi raqamni kiriting")
+
+
+alert(a + " + " + b + " = " + (a + b));
+// alert(a + " - " + b + " = " + (a - b));
+// alert(a + " * " + b + " = " + (a * b));
+// alert(a + " / " + b + " = " + (a / b));
